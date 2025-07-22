a666ed80056639527a6920a2b7cd7781e039711c
chenyipei <1351049203@qq.com>
2025-07-22 18:12:22 +0800

    0722 测试

--- test/test_1.py
+++ test/test_1.py
@@ -1,25 +1,14 @@
 from src.utils.scheduler import Scheduler
-from src.models.task import TASK_TYPE_INBOUND, TASK_TYPE_OUTBOUND, Task
+from src.models.task import TASK_TYPE_INBOUND, TASK_TYPE_OUTBOUND, TransportTask
 from src.models.vehicle import VEHICLE_TYPE_EMPTY, Vehicle
 
 if __name__ == "__main__":
     scheduler = Scheduler(num_vehicles=2, step_size=3, output_path="output/test_1")
     scheduler.grid.load_map_from_xlsx("resource/test_map.xlsx")
-    scheduler.task_manager.add_task(Task(
-        id="T1",
-        start_position=(8,7),
-        end_position=(10,7),
-        task_type=TASK_TYPE_OUTBOUND
-    ))
-    scheduler.task_manager.add_task(Task(
-        id="T2",
-        start_position=(10,7),
-        end_position=(12,7),
-        task_type=TASK_TYPE_INBOUND
-    ))
-
+    scheduler.task_manager.add_task(task_type=TASK_TYPE_OUTBOUND,start_pos=(10,6),end_pos=(18,15))
+    scheduler.task_manager.add_task(task_type=TASK_TYPE_INBOUND,start_pos=(2,8),end_pos=(3,6))
     # 添加车辆
-    vehicle_position = [(8,7), (10,7), (12,7), (14,7), (16,7), (18,7)]
+    vehicle_position = [(6,7), (8,7)]
     for i in range(scheduler.num_vehicles):
         v = Vehicle(
                 id=f"V{i+1}",
